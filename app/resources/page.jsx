d35c26956fa1ee2d2c97221060a91552b769dede
am225723 <puertaale@gmail.com>
2026-05-15 20:13:47 -0400

    Add Next.js resources page

---
+++ app/resources/page.jsx
@@ -0,0 +1,3 @@
+import ResourcesClient from '../../components/ResourcesClient';
+export const metadata = { title: 'Resources & FAQ | Integrative Psychiatry' };
+export default function ResourcesPage() { return <ResourcesClient />; }
