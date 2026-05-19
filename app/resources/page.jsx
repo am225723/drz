92c619c3283c57cbeb91f17e1310274f38a87825
am225723 <puertaale@gmail.com>
2026-05-19 10:55:21 -0400

    Add FAQPage JSON-LD to resources page

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,3 +1,29 @@
 import ResourcesClient from '../../components/ResourcesClient';
+import { FAQ_GROUPS } from '../../lib/faq';
+
 export const metadata = { title: 'Resources & FAQ | Integrative Psychiatry' };
-export default function ResourcesPage() { return <ResourcesClient />; }
+
+function buildFaqSchema() {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: FAQ_GROUPS.flatMap((group) => group.items).map(([question, answer]) => ({
+      '@type': 'Question',
+      name: question,
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: answer
+      }
+    }))
+  };
+}
+
+export default function ResourcesPage() {
+  return <>
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(buildFaqSchema()) }}
+    />
+    <ResourcesClient />
+  </>;
+}
