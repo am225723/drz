2158bbfb352d09faddba1acb8043f15280e030f7
am225723 <puertaale@gmail.com>
2026-05-20 11:08:35 -0400

    Add admin submissions list route

---
+++ app/admin/submissions/page.jsx
@@ -0,0 +1,7 @@
+import { SubmissionsList } from '../../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin Submissions | Integrative Psychiatry' };
+
+export default function AdminSubmissionsPage() {
+  return <SubmissionsList />;
+}
