ecf2ab3ca7fcf8fa0c7557e73acf8b63008d6b5b
am225723 <puertaale@gmail.com>
2026-05-20 11:20:14 -0400

    Add admin FAQ editor route

---
+++ app/admin/faq/page.jsx
@@ -0,0 +1,7 @@
+import { FaqsAdmin } from '../../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin FAQ | Integrative Psychiatry' };
+
+export default function AdminFaqPage() {
+  return <FaqsAdmin />;
+}
