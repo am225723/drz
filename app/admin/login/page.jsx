1c30c5b9ec2bf9d30dee7f94d42c03b95271f71e
am225723 <puertaale@gmail.com>
2026-05-20 10:58:57 -0400

    Add admin login route

---
+++ app/admin/login/page.jsx
@@ -0,0 +1,7 @@
+import { AdminLogin } from '../../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin Login | Integrative Psychiatry' };
+
+export default function AdminLoginPage() {
+  return <AdminLogin />;
+}
