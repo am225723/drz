127998fec20387d69df23e6c5c5e3862011fee4a
am225723 <puertaale@gmail.com>
2026-05-20 11:14:26 -0400

    Add admin pages route

---
+++ app/admin/pages/page.jsx
@@ -0,0 +1,7 @@
+import { PagesAdmin } from '../../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin Pages | Integrative Psychiatry' };
+
+export default function AdminPagesPage() {
+  return <PagesAdmin />;
+}
