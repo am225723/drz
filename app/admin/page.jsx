62a72d1862549e007619622790fd171f128e1b68
am225723 <puertaale@gmail.com>
2026-05-20 11:07:16 -0400

    Add admin dashboard route

---
+++ app/admin/page.jsx
@@ -0,0 +1,7 @@
+import { AdminHome } from '../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin Dashboard | Integrative Psychiatry' };
+
+export default function AdminPage() {
+  return <AdminHome />;
+}
