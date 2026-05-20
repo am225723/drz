0ecf7ce203e62802d3b9ae9d9c2b2a282e69374c
am225723 <puertaale@gmail.com>
2026-05-20 11:10:50 -0400

    Add admin articles route

---
+++ app/admin/articles/page.jsx
@@ -0,0 +1,7 @@
+import { ArticlesAdmin } from '../../../components/admin/AdminComponents';
+
+export const metadata = { title: 'Admin Articles | Integrative Psychiatry' };
+
+export default function AdminArticlesPage() {
+  return <ArticlesAdmin />;
+}
